eeb662b3dd37f0c58a0c512cd7101ce3a0bb2dc3
biaocy <biaocy91@gmail.com>
2024-01-16 02:48:46 +0800

    NVIM: remove telescope locked

--- .config/nvim/lua/core/plugins.lua
+++ .config/nvim/lua/core/plugins.lua
@@ -23,7 +23,6 @@ return require('packer').startup(function(use)
   use 'nvim-treesitter/nvim-treesitter'
   use {
     'nvim-telescope/telescope.nvim',
-    tag = '0.1.1',
     requires = {
       {'nvim-lua/plenary.nvim'},
     }
